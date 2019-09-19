0f8bfc6d596e9bc88f3278868c58bc5a5ca64a6f
Alejandro Pascual <alejandropascualpozo@gmail.com>
2019-09-19 18:05:17 +0200

    Completados alu_control y control_unit

--- p1/rtl/processor.vhd
+++ p1/rtl/processor.vhd
@@ -10,19 +10,19 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
 entity processor is
-   port(
-      clk        : in  std_logic; -- Reloj activo en flanco subida
-      reset      : in  std_logic; -- Reset asincrono activo nivel alto
-      -- ins memory
-      im_dir     : out std_logic_vector(31 downto 0); -- Direccion Instruccion
-      im_ins     : in  std_logic_vector(31 downto 0); -- Instruccion leida
-      -- Data memory
-      dm_dir     : out std_logic_vector(31 downto 0); -- Direccion
-      dm_rd_en   : out std_logic;                     -- Habilitacion lectura
-      dm_wr_en   : out std_logic;                     -- Habilitacion escritura
-      dm_data_wr : out std_logic_vector(31 downto 0); -- Dato escrito
-      dm_data_rd : in  std_logic_vector(31 downto 0)  -- Dato leido
-   );
+  port(
+    clk        : in  std_logic; -- Reloj activo en flanco subida
+    reset      : in  std_logic; -- Reset asincrono activo nivel alto
+    -- ins memory
+    im_dir     : out std_logic_vector(31 downto 0); -- Direccion Instruccion
+    im_ins     : in  std_logic_vector(31 downto 0); -- Instruccion leida
+    -- Data memory
+    dm_dir     : out std_logic_vector(31 downto 0); -- Direccion
+    dm_rd_en   : out std_logic;                     -- Habilitacion lectura
+    dm_wr_en   : out std_logic;                     -- Habilitacion escritura
+    dm_data_wr : out std_logic_vector(31 downto 0); -- Dato escrito
+    dm_data_rd : in  std_logic_vector(31 downto 0)  -- Dato leido
+  );
 end processor;
 
 architecture rtl of processor is 
