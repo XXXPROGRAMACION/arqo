ce4102003721b794451371d0b6219456bb52a2e2
Victor Yrazusta <victoryrazustaibarra@gmail.com>
2019-11-26 00:31:55 -0800

    Pasando cosas raras...

--- slow_fast_time.sh
+++ slow_fast_time.sh
@@ -9,14 +9,14 @@ n_repeticiones=10
 n_inicio=$((10000+1024*p))
 tam_paso=64
 n_final=$((n_inicio+(n_pasos-1)*tam_paso))
-f_dat=slow_fast_time.dat
-f_png=slow_fast_time.png
+f_dat=slow_fast_time
+f_png=slow_fast_time
 
 # borrar el fichero DAT y el fichero PNG
-rm -f $f_dat f_png
+rm -f $f_dat.dat f_png.png
 
 # generar el fichero DAT vacío
-touch $f_dat
+touch $f_dat.dat
 
 echo "Running slow and fast..."
 # bucle para n desde P hasta Q 
@@ -26,18 +26,15 @@ for ((r = 1; r <= n_repeticiones; r += 1)); do
 	for ((n = n_inicio ; n <= n_final ; n += tam_paso)); do
 		echo " -> n: $n / $n_final..."
 		
-		# ejecutar los programas slow y fast consecutivamente con tamaño de matriz n
-		# para cada uno, filtrar la línea que contiene el tiempo y seleccionar la
-		# tercera columna (el valor del tiempo). Dejar los valores en variables
-		# para poder imprimirlos en la misma línea del fichero de datos
 		slowTime=$(./slow $n | grep 'time' | awk '{print $3}')
 		fastTime=$(./fast $n | grep 'time' | awk '{print $3}')
 
-		echo "$n	$slowTime	$fastTime" >> $f_dat
+		echo "$n	$slowTime	$fastTime" >> $f_dat.dat
 	done
 done
 
-python3 slow_fast_media.py slow_fast_time.dat
+
+python3 slow_fast_media.py $f_dat.dat
 
 echo "Generating plot..."
 # llamar a gnuplot para generar el gráfico y pasarle directamente por la entrada
@@ -49,9 +46,9 @@ set xlabel "Matrix Size"
 set key right bottom
 set grid
 set term png
-set output "$f_png"
-plot "$f_dat" using 1:2 with lines lw 2 title "slow", \
-     "$f_dat" using 1:3 with lines lw 2 title "fast"
+set output "$f_png.png"
+plot "$f_dat.dat" using 1:2 with lines lw 2 title "slow", \
+     "$f_dat.dat" using 1:3 with lines lw 2 title "fast"
 replot
 quit
 END_GNUPLOT
